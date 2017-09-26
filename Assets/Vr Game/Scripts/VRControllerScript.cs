814fcb2afc440c92734898ac881426107537ae20
OlavDeng2 <olavkruithof@gmail.com>
2017-09-26 13:41:44 +0200

    started work on the Shooty thingie

---
+++ Assets/Vr Game/Scripts/VRControllerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class VRControllerScript : MonoBehaviour {
+
+    public Vector3 HandPosition;
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        HandPosition = InputTracking.GetLocalPosition(VRNode.RightHand);
+
+    }
+}
