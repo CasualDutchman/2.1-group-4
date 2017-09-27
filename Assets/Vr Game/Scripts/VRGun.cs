c579994f1b246dcc9880e25a7b7a5c933d8e4042
OlavDeng2 <olavkruithof@gmail.com>
2017-09-27 13:15:33 +0200

    added some code for the VRGun

---
+++ Assets/Vr Game/Scripts/VRGun.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRGun : MonoBehaviour {
+
+    private Vector3 VRGunRotation;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = GameObject.Find("VRControllerRight").GetComponent<Transform>().position;
+        transform.rotation = GameObject.Find("VRControllerRight").GetComponent<Transform>().rotation;
+    }
+}
