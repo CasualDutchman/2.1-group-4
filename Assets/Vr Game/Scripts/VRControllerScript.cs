1d6b25e24e495696249397e8d0020e70bf74424a
OlavDeng2 <olavkruithof@gmail.com>
2017-09-29 11:43:01 +0200

    Controller is now being tracked, but if player moves controller position does not move with

--- Assets/Vr Game/Scripts/VRControllerScript.cs
+++ Assets/Vr Game/Scripts/VRControllerScript.cs
@@ -5,7 +5,11 @@ using UnityEngine.VR;
 
 public class VRControllerScript : MonoBehaviour {
 
+    //variables for the position and rotation of the controllers
+    Vector3 PlayerPosition;
     Vector3 HandPosition;
+    Quaternion HandRotation;
+
     public string Hand = null;
     // Use this for initialization
     void Start () {
@@ -14,16 +18,26 @@ public class VRControllerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(Hand == "Right")
+        PlayerPosition = gameObject.GetComponentInParent<Transform>().position;
+
+
+        if (Hand == "Right")
         {
             HandPosition = InputTracking.GetLocalPosition(VRNode.RightHand);
-            transform.position = HandPosition;
+            transform.position = HandPosition;// + PlayerPosition;
+
+            HandRotation = InputTracking.GetLocalRotation(VRNode.RightHand);
+            transform.rotation = HandRotation;
         }
 
         if (Hand == "Left")
         {
             HandPosition = InputTracking.GetLocalPosition(VRNode.LeftHand);
-            transform.position = HandPosition;
+            transform.position = HandPosition;// + PlayerPosition;
+
+            HandRotation = InputTracking.GetLocalRotation(VRNode.LeftHand);
+            transform.rotation = HandRotation;
         }
+        
     }
 }
