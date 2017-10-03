26bf24c01ec354b70817e767d75077614ded5553
OlavDeng2 <olavkruithof@gmail.com>
2017-10-03 21:20:25 +0200

    New player prefab as well as controllers implemented

--- Assets/Vr Game/Scripts/SteamVRController.cs
+++ Assets/Vr Game/Scripts/SteamVRController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(SteamVR_TrackedObject))]
 public class SteamVRController : MonoBehaviour {
 
+    //Create variables for tracking the device and manipulation
     SteamVR_Controller.Device Device;
     SteamVR_TrackedObject trackedObj;
 
@@ -15,12 +16,45 @@ public class SteamVRController : MonoBehaviour {
 	
 	void Update ()
     {
+        //assign the device for input
         Device = SteamVR_Controller.Input((int)trackedObj.index);
 
-
+        //get the trigger being pulled
         if(Device.GetHairTriggerDown())
         {
-            Debug.Log("Trigger was pulled");
+            //TODO Implement firing
+            Debug.Log("Trigger was pulled and gun was fired");
         }
 	}
+
+    private void OnTriggerStay(Collider Col)
+    {
+        PickUpObject(Col);
+
+    }
+
+    //Function for picking up any object
+    private void PickUpObject(Collider Col)
+    {
+        //get the device input to pickup an object(using the trigger)
+        if (Device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            //Get position and transformation of the object picked up
+            Col.gameObject.transform.position = trackedObj.transform.position;
+            Col.gameObject.transform.rotation = trackedObj.transform.rotation;
+
+            //Make sure the object is kinematic
+            Col.attachedRigidbody.isKinematic = true;
+
+            //Set the parent of the object to the SteamVR controller to make transformation easier
+            Col.gameObject.transform.SetParent(gameObject.transform);
+        }
+
+        //Get input from device to drop object (The grip button)
+        if (Device.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+        {
+            //Set the parent of the object as the player
+            Col.gameObject.transform.SetParent(gameObject.transform.parent);
+        }
+    }
 }
