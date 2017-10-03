189173b1813f24269218bd45eedbeaf94be3462a
OlavDeng2 <olavkruithof@gmail.com>
2017-10-03 19:01:33 +0200

    added trigger pull and confirmed it works

---
+++ Assets/Vr Game/Scripts/PickupParent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class PickupParent : MonoBehaviour {
+
+    SteamVR_TrackedObject trackedObj;
+
+	void Awake ()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	void Update ()
+    {
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
+
+
+        if(device.GetHairTriggerDown())
+        {
+            Debug.Log("Trigger was pulled");
+        }
+	}
+}
