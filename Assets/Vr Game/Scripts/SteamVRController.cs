c23e0c98492ce8d5f1f0856df891c51607b28c99
OlavDeng2 <olavkruithof@gmail.com>
2017-10-04 15:35:26 +0200

    Made the gun Fire

--- Assets/Vr Game/Scripts/SteamVRController.cs
+++ Assets/Vr Game/Scripts/SteamVRController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(SteamVR_TrackedObject))]
 public class SteamVRController : MonoBehaviour {
 
+    public GameObject BulletPreFab;
+    GameObject Bullet;
+
     //Create variables for tracking the device and manipulation
     SteamVR_Controller.Device Device;
     SteamVR_TrackedObject trackedObj;
@@ -19,18 +22,17 @@ public class SteamVRController : MonoBehaviour {
         //assign the device for input
         Device = SteamVR_Controller.Input((int)trackedObj.index);
 
-        //get the trigger being pulled
-        if(Device.GetHairTriggerDown())
-        {
-            //TODO Implement firing
-            Debug.Log("Trigger was pulled and gun was fired");
-        }
 	}
 
     private void OnTriggerStay(Collider Col)
     {
         PickUpObject(Col);
 
+        //get the trigger being pulled
+        if (Device.GetHairTriggerDown() && Col.name == "Gun")
+        {
+            FireGun();
+        }
     }
 
     //Function for picking up any object
@@ -56,5 +58,13 @@ public class SteamVRController : MonoBehaviour {
             //Set the parent of the object as the player
             Col.gameObject.transform.SetParent(gameObject.transform.parent);
         }
+    }
+
+    private void FireGun()
+    {
+        //TODO implement gun firing
+        Debug.Log("Gun is fired");
+
+        Bullet = Instantiate(BulletPreFab, trackedObj.transform.position, trackedObj.transform.rotation);
     }
 }
