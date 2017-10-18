dfd0a28cbb987f45ea1aff19408ebf9a96c8907e
OlavDeng2 <olavkruithof@gmail.com>
2017-10-18 10:16:52 +0200

    Splitting profileManager and the UI for the ProfileManager scripts

---
+++ Assets/Vr Game/Scripts/ProfileUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfileUI : MonoBehaviour {
+    //UI Elements
+    public Canvas profileCanvas = null;
+
+    //drop down for the profile number
+    public Dropdown profileNumberDropdown;
+
+    //input field for the name
+    public InputField nameInputField;
+
+    //Buttons for loading and saving the data
+    public Button save, load;
+
+    //Sliders for the difficulties and the attention span
+    public Slider bikeDifficultySlider, gameDifficultySlider, attentionSpanSlider;
+
+    //Dropdown menu for the hand dominance
+    public Dropdown handDominanceMenu;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
